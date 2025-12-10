9ea024f021d89a2dd2c9816c720311272bce6b35
user <user@DESKTOP-PS1R8IB>
2025-12-10 21:11:30 +0900

    Alice에서 수정

--- GitHW/src/mygit/A.java
+++ GitHW/src/mygit/A.java
@@ -3,7 +3,7 @@ package mygit;
 public class A {
 
 	public A() {
-		
+		System.out.println("Alice에서 A 변경");
 	}
 
 }
--- GitHW/src/mygit/C.java
+++ GitHW/src/mygit/C.java
@@ -2,8 +2,10 @@ package mygit;
 
 public class C {
 
-	public C() {
-
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Alice에서 C 변경");
+		A a = new A();
 	}
 
 }
